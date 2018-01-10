d53c537def88fa4078b01735d58b613bbb4f2011
李启文 <selfassu@gmail.com>
2018-01-10 17:38:55 +0800

    新增一些 content 方法

--- blog-web/src/main/java/com/itqiwen/blog/web/controller/admin/ContentController.java
+++ blog-web/src/main/java/com/itqiwen/blog/web/controller/admin/ContentController.java
@@ -1,16 +1,25 @@
 package com.itqiwen.blog.web.controller.admin;
 
 import com.itqiwen.blog.config.Config;
+import com.itqiwen.blog.config.ContentState;
+import com.itqiwen.blog.config.ContentType;
 import com.itqiwen.blog.domain.Content;
+import com.itqiwen.blog.domain.RestResponse;
 import com.itqiwen.blog.service.ContentService;
+import com.itqiwen.blog.utils.DateUtils;
 import com.itqiwen.blog.utils.JsonUtil;
 import com.itqiwen.blog.web.controller.BaseController;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,6 +29,8 @@ import java.util.List;
 @RequestMapping("admin")
 @RestController
 public class ContentController extends BaseController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContentController.class);
 
 
     @Resource
@@ -39,8 +50,66 @@ public class ContentController extends BaseController {
         }
         Page<Content> pageContent = contentService.findContentByCriteria(page, Config.PAGE_SIZE);
         List<Content> contentList = pageContent.getContent();
-        System.out.println("当前数据：" + contentList.size());
-        System.out.println("当前页码：" + pageContent.getTotalPages());
         return JsonUtil.toJSONString(contentList);
+    }
+
+
+    /**
+     * 预览文章，将文章存入草稿，进行预览，
+     * 预览过后，用户确定发布的话，将文章状态更改成 发布
+     * @param content
+     * @return
+     */
+    @RequestMapping(value = "/content/preview", method = RequestMethod.POST)
+    public String previewContent(Content content, HttpServletRequest request){
+        LOGGER.info("starting preview content");
+        content.setType(ContentType.PUBLIC.getType());
+        content.setState(ContentState.DRAFT.getState());
+        content.setCreateDt(DateUtils.getUnixTimeByDate(new Date()));
+        content.setUpdateDt(DateUtils.getUnixTimeByDate(new Date()));
+        //如果摘要不存在，截取文章正文部分 前 100 个字符作为摘要
+        if(StringUtils.isEmpty(content.getDigest())){
+            //判断文章长度，如果文章正文长度大于100， 截取 100
+            if(content.getContentMd().length() > 100){
+                content.setDigest(content.getContentMd().substring(0, 100));
+            }else{
+                //小于 100 ，全部赋值给 正文
+                content.setDigest(content.getContentMd());
+            }
+        }
+        contentService.saveContent(content);
+        request.setAttribute("content", content);
+        return "common/post";
+    }
+
+    /**
+     * 新增内容
+     * @return
+     */
+    @RequestMapping(value = "/content/new", method = RequestMethod.GET)
+    public String newContent(){
+        return "admin/content_new";
+    }
+
+    /**
+     * 用户不预览，直接发布内容
+     * @param content
+     * @return
+     */
+    @RequestMapping(value = "/content/new", method = RequestMethod.POST)
+    public RestResponse newContent(Content content){
+
+        return RestResponse.ok();
+    }
+
+    /**
+     * 修改文章状态
+     * @param state
+     * @return
+     */
+    @RequestMapping(value = "/content/update", method = RequestMethod.GET)
+    public String updateContentState(@PathVariable("state")String state){
+
+        return null;
     }
 }
